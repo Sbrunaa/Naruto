483364c1d3856ac532ec2413237e11420abe9c05
Bruna Santos <83190947+Sbrunaa@users.noreply.github.com>
2024-09-06 18:34:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ function pesquisar(){
     //obtém a seção html onde os resultados serão exibidos.
     let section = document.getElementById("resultado-pesquisa");
 
+    //obtem as informaçãos escrita no campo pesquisa
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    //se campoPesquisa for uma string vazia
+    if(!campoPesquisa) {
+        section.innerHTML ="<p.Você precisa digitar o nome do personagem ou seu clã.</p>"
+        section.innerHTML ="<p>nada foi encontrado</p>"
+        return
+    }
+    
+    //faz com que as letras fiquem minusculas
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     //a string inicializaa vazia para armazenar os resltados 
     let resultados = "";
+    let titulo ="";
+    let vila ="";
 
     //itera(ou obtém) cada dadoo da lista de dados
     for(let dado of dados) {
-        //cria um novo elemento html para cada resultado
-        resultados +=`
+        titulo = dado.titulo.toLowerCase()
+        vila = dado.vila.toLowerCase()
+        if(titulo.includes(campoPesquisa) || vila.includes(campoPesquisa)) {
+            //cria um novo elemento html para cada resultado
+            resultados +=`
             <div class="item-resultado">
                 <div class="card mb-3" style="max-width: 540px">
                     <div class="row g-0">
@@ -28,7 +46,13 @@ function pesquisar(){
                 </div>
             </div>
         `;
+        }   
     }
+    //verifica se o que foi pesquisa tem na base de dados
+    if (!resultados) {
+        resultados ="<p>Nada foi encontrado</p>"
+    }
+
     //atribtui os resultados gerados na seção html
     section.innerHTML = resultados;
 }
